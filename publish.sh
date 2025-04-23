f430d1de6a6cce2628f45f5aa73d9396beacaf0a
Hai Liang Wang <hai@chatopera.com>
2025-04-23 17:18:22 +0800

    init

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir
+rm -rf ./dist/*
+# python setup.py sdist upload -r pypi
+python setup.py sdist
+twine upload --repository pypi dist/*
+
+if [ $? -eq '0' ]; then
+    rm -rf *.egg-info/
+fi
